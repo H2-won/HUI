174ba48b045d41e98bf36b21b6238d9e648d77b7
LIMHUIWON <59960678+LIMHUIWON@users.noreply.github.com>
2020-08-20 01:24:41 +0900

    burning 효과 추가

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,6 @@
 document.querySelector("#menu-btn").addEventListener("click",function(){
     document.querySelector(".nav-pop").classList.toggle("active");
 })
+
+document.querySelector(".p4").attributes[1].nodeValue = document.querySelector(".p4").childNodes[0].textContent;
+
