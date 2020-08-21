4ca8d99dd3a5d99889255d78b1a8171f74e7a6a8
LIMHUIWON <59960678+LIMHUIWON@users.noreply.github.com>
2020-08-21 15:34:02 +0900

    background image 변경

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -4,3 +4,14 @@ document.querySelector("#menu-btn").addEventListener("click",function(){
 
 document.querySelector(".p4").attributes[1].nodeValue = document.querySelector(".p4").childNodes[0].textContent;
 
+document.querySelector("#p1").addEventListener("mouseover",function(){
+    document.querySelector(".second-back").style.backgroundImage = "url('../static/img/mansbuy-back.jpg')"
+})
+
+document.querySelector("#p2").addEventListener("mouseover",function(){
+    document.querySelector(".second-back").style.backgroundImage = "url('../static/img/calendar-back.jpg')"
+})
+
+document.querySelector("#p3").addEventListener("mouseover",function(){
+    document.querySelector(".second-back").style.backgroundImage = "url('../static/img/mansbuy-back.jpg')"
+})
