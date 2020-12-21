1ea99e8f7d72369315cc42bc213cdece7baaf0c4
LIMHUIWON <59960678+LIMHUIWON@users.noreply.github.com>
2020-12-22 00:58:21 +0900

    eye animation ver0 (20.12.22)

--- src/static/js/parallax.js
+++ src/static/js/parallax.js
@@ -9,3 +9,36 @@ function parallax(e) {
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
     });
 }
+
+document.querySelector('body').addEventListener('mousemove', eyeball);
+function eyeball(){
+    const eye = document.querySelectorAll('.eye');
+    eye.forEach( eye => {
+        let x = (eye.getBoundingClientRect().left) + (eye.clientHeight/2);
+        let y = (eye.getBoundingClientRect().top) + (eye.clientHeight/2);
+
+        let radian = Math.atan2(event.pageX -x, event.pageY -y);
+        let rotation = (radian * (180/Math.PI) * -1) + 270;
+        eye.style.transform =  'rotate('+rotation+'deg)';
+    });
+}
+
+const halloweenTitle = document.querySelector('section h2');
+const eyeBall = document.querySelectorAll('.eyeBall');
+const cursor = document.querySelector('.cursor');
+halloweenTitle.addEventListener('mouseover', ()=>{
+    halloweenTitle.style.color = '#62C110';
+    eyeBall[0].style.border = '10px solid #62C110';
+    eyeBall[1].style.border = '10px solid #62C110';
+    cursor.style.border = '2.2px solid #62C110';
+    cursor.style.width = '5em';
+    cursor.style.height = '5em';
+})
+halloweenTitle.addEventListener('mouseout', ()=>{
+    halloweenTitle.style.color = '#FF6912';
+    eyeBall[0].style.border = '10px solid #FF6912';
+    eyeBall[1].style.border = '10px solid #FF6912';
+    cursor.style.border = '4px solid #FF6912';
+    cursor.style.width = '0';
+    cursor.style.height = '0';
+})
