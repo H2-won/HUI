64b5708399727e348b64b1d367cee3301d55a230
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2022-01-06 05:03:35 +0900

    Update cursor.js

--- src/static/js/cursor.js
+++ src/static/js/cursor.js
@@ -1,26 +1,33 @@
-const cursor = document.querySelector('.cursor');
-const logo = document.querySelector('.icon');
+const cursor = document.querySelector(".cursor");
+const logo = document.querySelector(".icon");
 
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
-})
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+});
 
-function mouseHovering(component, text = '', hoverColor = '#F63E61', hoverSize = '5em', dotSize = '4px') {
-    component.addEventListener('mouseover', () => {
-        cursor.innerText = text;
-        cursor.style.border = `2.2px solid ${hoverColor}`;
-        cursor.style.width = hoverSize;
-        cursor.style.height = hoverSize;
-    })
+function mouseHovering(
+  component,
+  text = "",
+  hoverColor = "#F63E61",
+  hoverSize = "5em",
+  dotSize = "4px"
+) {
+  component.addEventListener("mouseover", () => {
+    cursor.innerText = text;
+    cursor.style.border = `2.2px solid ${hoverColor}`;
+    cursor.style.color = hoverColor;
+    cursor.style.width = hoverSize;
+    cursor.style.height = hoverSize;
+  });
 
-    component.addEventListener('mouseout', () => {
-        const cursorColor = cursor.getAttribute('color');
-        cursor.style.border = `${dotSize} solid ${cursorColor}`;
-        cursor.innerText = '';
-        cursor.style.width = '0';
-        cursor.style.height = '0';
-    })
+  component.addEventListener("mouseout", () => {
+    const cursorColor = cursor.getAttribute("color");
+    cursor.style.border = `${dotSize} solid ${cursorColor}`;
+    cursor.innerText = "";
+    cursor.style.width = "0";
+    cursor.style.height = "0";
+  });
 }
 
-mouseHovering(logo, '', '#F63E61', '5em');
+mouseHovering(logo, "", "#F63E61", "5em");
