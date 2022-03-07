e46897efca3f01443ff67b20067ff92c022f24dc
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2022-03-07 23:49:32 +0900

    Add Icon in birthdayLetter, set single quote prettier

--- src/static/js/birthdayLetter.js
+++ src/static/js/birthdayLetter.js
@@ -1,21 +1,21 @@
-const envelope = document.querySelector(".envelope"),
-  cardPreview = document.querySelector(".cardPreview"),
-  card = document.querySelector(".card"),
-  background = document.querySelector(".background");
+const envelope = document.querySelector('.envelope'),
+  cardPreview = document.querySelector('.cardPreview'),
+  card = document.querySelector('.card'),
+  background = document.querySelector('.background');
 
-envelope.addEventListener("click", () => {
-  cardPreview.classList.add("moveUp");
+envelope.addEventListener('click', () => {
+  cardPreview.classList.add('moveUp');
   setTimeout(() => {
-    background.style.webkitTransform = "matrix(10, 0, 0, 10, 0, 0)";
+    background.style.webkitTransform = 'matrix(10, 0, 0, 10, 0, 0)';
   }, 1250);
 
   setTimeout(() => {
-    card.style.display = "flex";
-    envelope.style.display = "none";
+    card.style.display = 'flex';
+    envelope.style.display = 'none';
   }, 1750);
 
   setTimeout(() => {
-    card.style.opacity = "1";
+    card.style.opacity = '1';
     createRandomBubbles();
     createContentController();
   }, 2250);
@@ -23,21 +23,21 @@ envelope.addEventListener("click", () => {
 
 function createRandomBubbles() {
   const cnt = 50,
-    section = document.querySelector(".card");
+    section = document.querySelector('.card');
   let i = 0;
   while (i < cnt) {
-    const createdBubble = document.createElement("i");
+    const createdBubble = document.createElement('i');
     const x = Math.floor(Math.random() * window.innerWidth);
     const y = Math.floor(Math.random() * window.innerHeight);
 
     const size = Math.random() * 5;
-    createdBubble.style.left = x + "px";
-    createdBubble.style.top = y + "px";
-    createdBubble.style.width = size + 1 + "px";
-    createdBubble.style.height = size + 1 + "px";
+    createdBubble.style.left = x + 'px';
+    createdBubble.style.top = y + 'px';
+    createdBubble.style.width = size + 1 + 'px';
+    createdBubble.style.height = size + 1 + 'px';
 
-    createdBubble.style.animationDuration = size + 3 + "s";
-    createdBubble.style.animationDelay = -size + "s";
+    createdBubble.style.animationDuration = size + 3 + 's';
+    createdBubble.style.animationDelay = -size + 's';
 
     section.appendChild(createdBubble);
     i++;
@@ -45,44 +45,44 @@ function createRandomBubbles() {
 }
 
 function createContentController() {
-  const contents = document.querySelectorAll(".content");
+  const contents = document.querySelectorAll('.content');
   let page = 0;
 
-  const controllers = document.createElement("div");
-  controllers.classList = "controllers";
+  const controllers = document.createElement('div');
+  controllers.classList = 'controllers';
 
-  const controllerLeft = document.createElement("div");
-  controllerLeft.classList = "controllerLeft";
-  controllerLeft.addEventListener("click", () => {
+  const controllerLeft = document.createElement('div');
+  controllerLeft.classList = 'controllerLeft';
+  controllerLeft.addEventListener('click', () => {
     if (page === 0) {
       return;
     } else if (page === 1) {
-      controllerLeft.classList.remove("active");
+      controllerLeft.classList.remove('active');
     }
-    contents[page].classList.remove("active");
+    contents[page].classList.remove('active');
     page -= 1;
     setTimeout(() => {
-      contents[page].classList.add("active");
+      contents[page].classList.add('active');
     }, 1000);
 
-    controllerRight.classList.add("active");
+    controllerRight.classList.add('active');
   });
 
-  const controllerRight = document.createElement("div");
-  controllerRight.classList = "controllerRight active";
-  controllerRight.addEventListener("click", () => {
+  const controllerRight = document.createElement('div');
+  controllerRight.classList = 'controllerRight active';
+  controllerRight.addEventListener('click', () => {
     if (page === contents.length - 1) {
       return;
     } else if (page === contents.length - 2) {
-      controllerRight.classList.remove("active");
+      controllerRight.classList.remove('active');
     }
-    contents[page].classList.remove("active");
+    contents[page].classList.remove('active');
     page += 1;
     setTimeout(() => {
-      contents[page].classList.add("active");
+      contents[page].classList.add('active');
     }, 1000);
 
-    controllerLeft.classList.add("active");
+    controllerLeft.classList.add('active');
   });
 
   controllers.append(controllerLeft, controllerRight);
--- src/static/js/parallax.js
+++ src/static/js/parallax.js
@@ -1,7 +1,7 @@
-document.addEventListener("mousemove", parallax);
+document.addEventListener('mousemove', parallax);
 function parallax(e) {
-  this.querySelectorAll(".layer").forEach((layer) => {
-    const speed = layer.getAttribute("data-speed");
+  this.querySelectorAll('.layer').forEach((layer) => {
+    const speed = layer.getAttribute('data-speed');
 
     const x = (window.innerWidth - e.pageX * speed) / 100;
     const y = (window.innerHeight - e.pageY * speed) / 100;
@@ -10,9 +10,9 @@ function parallax(e) {
   });
 }
 
-document.querySelector("body").addEventListener("mousemove", eyeball);
+document.querySelector('body').addEventListener('mousemove', eyeball);
 function eyeball() {
-  const eye = document.querySelectorAll(".eye");
+  const eye = document.querySelectorAll('.eye');
   eye.forEach((eye) => {
     let x = eye.getBoundingClientRect().left + eye.clientHeight / 2;
     let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
@@ -24,24 +24,24 @@ function eyeball() {
 }
 
 function halloweenTitleHovering() {
-  const halloweenTitle = document.querySelector("section h2");
-  const eyeBall = document.querySelectorAll(".eyeBall");
-  const cursor = document.querySelector(".cursor");
-  halloweenTitle.addEventListener("mouseover", () => {
-    halloweenTitle.style.color = "#62C110";
-    eyeBall[0].style.border = "10px solid #62C110";
-    eyeBall[1].style.border = "10px solid #62C110";
-    cursor.style.border = "2.2px solid #62C110";
-    cursor.style.width = "5em";
-    cursor.style.height = "5em";
+  const halloweenTitle = document.querySelector('section h2');
+  const eyeBall = document.querySelectorAll('.eyeBall');
+  const cursor = document.querySelector('.cursor');
+  halloweenTitle.addEventListener('mouseover', () => {
+    halloweenTitle.style.color = '#62C110';
+    eyeBall[0].style.border = '10px solid #62C110';
+    eyeBall[1].style.border = '10px solid #62C110';
+    cursor.style.border = '2.2px solid #62C110';
+    cursor.style.width = '5em';
+    cursor.style.height = '5em';
   });
-  halloweenTitle.addEventListener("mouseout", () => {
-    halloweenTitle.style.color = "#FF6912";
-    eyeBall[0].style.border = "10px solid #FF6912";
-    eyeBall[1].style.border = "10px solid #FF6912";
-    cursor.style.border = "4px solid #FF6912";
-    cursor.style.width = "0";
-    cursor.style.height = "0";
+  halloweenTitle.addEventListener('mouseout', () => {
+    halloweenTitle.style.color = '#FF6912';
+    eyeBall[0].style.border = '10px solid #FF6912';
+    eyeBall[1].style.border = '10px solid #FF6912';
+    cursor.style.border = '4px solid #FF6912';
+    cursor.style.width = '0';
+    cursor.style.height = '0';
   });
 }
 halloweenTitleHovering();
